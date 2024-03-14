cb9ce62b8dcaa2c3ed29b1f5a8fc227e68ea23c2
Natálie Slavíková <slavikova.natalie@centrum.cz>
2024-03-14 14:15:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+/* Mějme tři cateringové společnosti dodávající občerstvení na různé akce. Jsou to
+
+Just Food: Dodává levné jídlo vyrobené z nepříliš kvalitních surovin.
+
+Your Mama: Dodává středně drahé jídlo vyrobené z lepších surovin.
+
+Flavour Haven: Dodává drahé jídlo z kvalitních surovin.
+
+Napište pro každou z těcho společností funkci s jedním parametrem, který udává, pro kolik lidí se catering objednává.
+ Funkce pak vrátí řetězec obsahující zprávu o tom že catering byl dodán a za jakou cenu. Například
+
+Catering od Flavour Haven pro 100 lidí za 300000 Kč
+
+Vyzkoušjte si vaše funkce v konzoli. 
+
+Poté napište funkci createEvent, která představuje vytvoření nějaké události s cateringem. 
+Funkce na vstupu obdrží název události, počet lidí a funkci, pomocí které se má provést catering.
+ Funkce createEvent pak vrátí zprávu ve smyslu
+
+Událost Inaugurace prezidenta s catering od Flavour Haven pro 100 lidí za 300000 Kč */
+
+const justFood = (people) => {
+    return `Catering od Just Food je pro ${people} lidí za 200 000 Kč`;
+}
+
+const yourMama = (people) => {
+    return `Catering od Your Mama je pro ${people} lidí za 200 000 Kč`;
+}
+
+const flavourHaven = (people) => {
+    return `Catering od Flavour Haven je pro ${people} lidí za 200 000 Kč`;
+}
+
+console.log(justFood(200));
+console.log(yourMama(120));
+console.log(flavourHaven(70));
+
+
+
+const createEvent = (event, people, catering) => {
+    return `Událost ${event} s cateringem od ${catering} je pro ${people} lidí za 200 000 Kč`;
+}
+
+console.log(createEvent("inaugurace prezidenta", 120, "Your Mama"));
